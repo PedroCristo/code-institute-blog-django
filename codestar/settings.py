fcda4fece88bed9645dc9f0a0c5d443599be4f87
Pedro Cristo <pcristo35@gmail.com>
2022-04-26 11:01:57 +0000

    Update ALLOEWD_HOSTS in settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['code-institute-django-blog.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['code-institute-django-blog.herokuapp.com', 'localhost']
 
 
 # Application definition
