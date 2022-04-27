8981f7ab1f50eb3658214a9e77b8c73dbc44eec8
Pedro Cristo <pcristo35@gmail.com>
2022-04-27 12:24:27 +0000

    Review code in the files

--- blog/admin.py
+++ blog/admin.py
@@ -6,11 +6,11 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status', 'created_on') 
-    search_fields = ('title', 'content')
-    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
